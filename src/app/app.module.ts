d1d853aebc7d5d2f39954178f57f80dae3f16b4b
Julián Pérez Pesce <julianperezpesce@users.noreply.github.com>
2020-05-25 21:24:16 -0300

    se crearon los componentes pedido y pedidoDetalle

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HijoComponent } from './hijo/hijo.component';
+import { PedidoComponent } from './pedido/pedido.component';
+import { PedidoDetalleComponent } from './pedido-detalle/pedido-detalle.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HijoComponent
+    HijoComponent,
+    PedidoComponent,
+    PedidoDetalleComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pedido-detalle/pedido-detalle.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pedido-detalle',
+  templateUrl: './pedido-detalle.component.html',
+  styleUrls: ['./pedido-detalle.component.scss']
+})
+export class PedidoDetalleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
