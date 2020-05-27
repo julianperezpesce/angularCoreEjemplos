aa4a58c370fac4054018e30d06d79c5c45b70054
Julián Pérez Pesce <julianperezpesce@users.noreply.github.com>
2020-05-26 21:13:02 -0300

    new pedido: Pedido

--- src/app/pedido/pedido.component.ts
+++ src/app/pedido/pedido.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Pedido } from '../models/pedido';
 
 @Component({
   selector: 'app-pedido',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PedidoComponent implements OnInit {
 
+  pedido: Pedido = new Pedido();
   constructor() { }
 
   ngOnInit(): void {
